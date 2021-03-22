d1fd78d2764d88b3670ed711ec59f1fec0b79d8f
eliezer <eliezeralves@rocketmail.com>
2021-03-22 13:48:04 -0300

    style commit

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,10 +1,45 @@
 class ApiController < ApplicationController
+  def index
+  end
+
   def users
+    url = "https://api.github.com/users"
+    client_id = 'Iv1.18206e9ce3d902e7'
+    client_secret = 'cf0d5b46bf68a2167d4b2515cf7ad1c2dbd1d447'
+    
+      #@list2 = HTTParty.get "#{url}?client_id=#{client_id}&client_secret=#{client_secret}"
+      @list2 = HTTParty.get "https://api.github.com/users?since=0&per_page=100"
+        
+      respond_to do |format|
+        format.html 
+        format.json 
+      end
+    
   end
 
   def repositories
+    url = "https://api.github.com/repositories"
+    client_id = 'Iv1.18206e9ce3d902e7'
+    client_secret = 'cf0d5b46bf68a2167d4b2515cf7ad1c2dbd1d447'
+    
+    @repo = HTTParty.get "#{url}?client_id=#{client_id}&client_secret=#{client_secret}"
+         
+    respond_to do |format|
+      format.html 
+      format.json 
+    end
   end
 
-  def index
+  def show
+    url = "https://api.github.com/users"
+    client_id = 'Iv1.18206e9ce3d902e7'
+    client_secret = 'cf0d5b46bf68a2167d4b2515cf7ad1c2dbd1d447'
+        
+    @user = HTTParty.get "#{url}/#{params[:id]}?client_id=#{client_id}&client_secret=#{client_secret}"
+       
+    respond_to do |format|
+      format.html 
+      format.json 
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'api#users'
   get 'api/repositories'
   get 'api/index'
+  resources :api
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
