4c180a6d73a11e2b7293a94961e508bac7a916da
eliezer <eliezeralves@rocketmail.com>
2021-03-22 13:57:16 -0300

    styles commit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'api#users'
   get 'api/repositories'
   get 'api/index'
-  resources :api
+  #resources :api
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
