4bed2b097dddb4996c5ed660951c05b4966fe502
eliezer <eliezeralves@rocketmail.com>
2021-03-22 14:23:57 -0300

    recover routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'api#users'
+  get 'api/users'
   get 'api/repositories'
-  get 'api/index'
+  root 'api#index'
   #resources :api
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
